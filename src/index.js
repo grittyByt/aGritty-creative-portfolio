a1b18f2d709f0c87eae51942288f6f49d8aaea6b
grittyByt <terrell_whiting@yahoo.com>
2024-03-28 10:57:19 -0400

    added a quote for every day of the week / currently cycling through each quote at aproximately every 4 seconds / next step to add padding to quote block so long quotes are not overtly stretched and make buttons visible for the carousel / page is ugly on purpose for now

--- src/index.js
+++ src/index.js
@@ -13,33 +13,30 @@ const authorContainer = [];
 const sourceContainer = [];
 const good = 0;
 
-quoteContainer.push('Computers are like Old Testament gods; lots of rules and no mercy.', 'Man is still the most extraordinary computer of all', 'The good news about computers is that they do what you tell them to do.  The bad news is they do what you tell them to do.')
-authorContainer.push('Joseph Campbell', 'President John F. Kennedy', 'Ted Nelson', 'Nate White');
-sourceContainer.push('Brainy Quote', 'Brainy Quote', 'Unknown');
+quoteContainer.push('Computers are like Old Testament gods; lots of rules and no mercy.', 
+                    'Man is still the most extraordinary computer of all', 
+                    'The good news about computers is that they do what you tell them to do.  The bad news is they do what you tell them to do.', 
+                    "Never trust a computer you can't throw out a window",
+                    'Computing is not about computers any more.  It is about living',
+                    'Those who can imagine anything, can create the impossible',
+                    'Good judgement comes from experience, and experience comes from bad judgement')
+authorContainer.push('Joseph Campbell', 'President John F. Kennedy', 'Ted Nelson', 'Steve Wozniack', 'Nicholas Negroponte', 'Alan Turing', 'Frederick P. Brooks');
+sourceContainer.push('Brainy Quote', 'The Power of Myth', 'Unknown');
 
 // object for quotes
-const quotes = [{quote: 'Computers are like Old Testament gods; lots of rules and no mercy.', author: 'Joseph Campbell', source: 'Brainy Quote'},
+const quotes = [{quote: 'Computers are like Old Testament gods; lots of rules and no mercy.', author: 'Joseph Campbell', source: 'The Power of Myth'},
 {quote: 'Man is still the most extraordinary computer of all', author: 'President John F. Kennedy', source: 'Brainy Quote'},
-{quote: 'The good news about computers is that they do what you tell them to do.  The bad news is they do what you tell them to do.', author: 'Ted Nelson', source: 'Unknown'}];
+{quote: 'The good news about computers is that they do what you tell them to do.  The bad news is they do what you tell them to do.', author: 'Ted Nelson', source: 'Unknown'},
+{quote: "Never trust a computer you can't throw out a window", author: 'Steve Wozniack', source: 'Brainy Quote'},
+{quote: 'Computing is not about computers any more.  It is about living', author: 'Nicholas Negroponte', source: 'Unknown'},
+{quote: 'Those who can imagine anything, can create the impossible', author: 'Alan Turing', source: 'Unknown'},
+{quote: 'Computer Science is no more about computers than astronomy is about telescopes', author: 'Edsger Wybe Dijkstra', source: 'Unknown'}];
 
 const the = quotes.map(theQuotes => theQuotes.quote);
 const by = quotes.map(byAuthors => byAuthors.author);
 const from = quotes.map(fromSources => fromSources.source);
 
-console.log(the[0]);
-// console.log(...by);
-// console.log(from);
 oneQuoteAtATime();
-
-
-function shuffleQuotes(quote){
-    console.log('here')
-    return quote();
-}
-
-// function displayEmpty(whereQuotesGo){
-//     setTimeout(() => {whereQuotesGo.innerText += ''}, 8000);
-// }
 
 function oneQuoteAtATime(){
     quoteArea.innerText += the[0];
@@ -47,19 +44,14 @@ function oneQuoteAtATime(){
     sourceTitleArea.innerText += from[0];
     
     let sid = setTimeout(() => {
-        
-        console.log('blah');
-        // immediately
-        // not really necessary, but I created the function as I was learning how to make the quotes change on their own time
+        // immediate
+        // necessary, I created the function as I was learning how to make the quotes change on their own time
         // function operation: it empties the whole quote block
         disappearQuote();
         // function operation: after one day passes (86400 secs) the next quote appears while the old one is removed from user sight
         nextQuoteAppear();
-       
     }, 5000);
-
     return sid;
-
 }
 
 // function quoteTwo(callThree){
@@ -185,23 +177,48 @@ function oneQuoteAtATime(){
 // const rotatingQuotes =
 
 function nextQuoteAppear(){
-    disappearQuote;
-    quoteArea.innerText = the[1];
-    authorArea.innerText = by[1];
-    sourceTitleArea.innerText = from[1];
-    console.log('2 secs');
+    disappearQuote();
+    quoteArea.innerText += the[1];
+    authorArea.innerText += by[1];
+    sourceTitleArea.innerText += from[1];
     setTimeout(() => {
-        disappearQuote;
-        quoteArea.innerText = the[2];
-        authorArea.innerText = by[2];
-        sourceTitleArea.innerText = from[2];
-        console.log('4 secs');
+        disappearQuote();
+        quoteArea.innerText += the[2];
+        authorArea.innerText += by[2];
+        sourceTitleArea.innerText += from[2];
+        setTimeout(() => {
+            disappearQuote();
+            quoteArea.innerText += the[3];
+            authorArea.innerText += by[3];
+            sourceTitleArea.innerText += from[3];
+            setTimeout(() => {
+                disappearQuote();
+                quoteArea.innerText += the[4];
+                authorArea.innerText += by[4];
+                sourceTitleArea.innerText += from[4];
+                setTimeout(() => {
+                    disappearQuote();
+                    quoteArea.innerText += the[5];
+                    authorArea.innerText += by[5];
+                    sourceTitleArea.innerText += from[5];
+                    setTimeout(() => {
+                        disappearQuote();
+                        quoteArea.innerText += the[6];
+                        authorArea.innerText += by[6];
+                        sourceTitleArea.innerText += from[6];
+                        setTimeout(() => {
+                            disappearQuote();
+                            return oneQuoteAtATime();
+                        }, 4000)
+                    }, 4000)
+                }, 4000)
+            }, 4000)
+        }, 4000)
     }, 4000)
 }
 
 
 function disappearQuote(){
-    console.log('disappear');
     quoteArea.innerText = '';
     authorArea.innerText = '';
     sourceTitleArea.innerText = '';
